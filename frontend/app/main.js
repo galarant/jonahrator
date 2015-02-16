d6e81773fbd4f96b08d59d7e708228158bc6eab7
Jeff <jeff.revesz@buzzfeed.com>
2015-02-15 22:49:03 -0500

    removed bad require

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -4,7 +4,6 @@ require('../bower_components/bootstrap/dist/js/bootstrap.js');
 require('../bower_components/angular/angular.js');
 require('../bower_components/angular-route/angular-route.js');
 require('./app.js');
-require('./routes.js');
 require('./controllers/home.js');
 require('./controllers/quote.js');
 
